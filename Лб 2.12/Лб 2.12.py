505532924453b8dcefd2449e24a69e77ed0187cf
Mitya_Moryakov <dim.maluckow2017@yandex.ru>
2024-04-21 22:15:01 +0300

    done 12_lab code

--- Лб 2.12/Лб 2.12.py
+++ Лб 2.12/Лб 2.12.py
@@ -1,39 +1,30 @@
 class Bakery():
-    def __init__(self, name='', count_products=0, avg_price=0):
-        self.__name = name
-        self.__count_products = count_products
-        self.__avg_price = avg_price
+    def __init__(self, name:str, count_products:int, avg_price: (int, float)):
+        self._name = name
+        self._count_products = count_products
+        self._avg_price = avg_price
 
-    def set_name(self, name):
-        if type(name) in str:
-            self.__name = name
-        else:
-            raise ValueError('Название пекарни должно иметь строковый тип')
+    def set_name(self, name:str):
+        self._name = name
 
-    def set_count_products(self, count_products):
-        if type(count_products) in int:
-            self.__count_products = count_products
-        else:
-            raise ValueError('Количество изделий должно быть целым числом')
+    def set_count_products(self, count_products:int):
+        self._count_products = count_products
 
-    def set_avg_price(self, avg_price):
-        if type(avg_price) in (int, float):
-            self.__avg_price = avg_price
-        else:
-            raise ValueError('Средняя цена на изделие должна быть либо целым,либо дробным числом')
+    def set_avg_price(self, avg_price:(int,float)):
+        self._avg_price = avg_price
 
     def get_name(self):
-        return self.__name
+        return self._name
 
     def get_count_product(self):
-        return self.__count_products
+        return self._count_products
 
     def get_avg_price(self):
-        return self.__avg_price
+        return self._avg_price
 
 
-a = Bakery('Игорь', 6, 1000)
+a = Bakery(6, 100, 1000)
 b = Bakery('Кулеш', 1, 500)
-# a.set_name('Кулеш')
 print(a.get_name(), a.get_count_product(), a.get_avg_price())
 print(b.get_name(), b.get_count_product(), b.get_avg_price())
+# a.set_name('Кулеш')
--- Лб 2.12/Лб тест.py
+++ Лб 2.12/Лб тест.py
@@ -1,16 +1,38 @@
-class Point:
-    def __init__(self, x, y):
-        self.__x = x
-        self.__y = y
-    def set_coord(self, x, y):
-        if type(x) in (int, float) and type(y) in (int, float):
-            self.__x = x
-            self.__y = y
+class Bakery():
+    def __init__(self, name: str, count_products: int, avg_price: (int, float)):
+        self.set_name(name)
+        self.set_count_products(count_products)
+        self.set_avg_price(avg_price)
+
+    def set_name(self, name: str):
+        if isinstance(name, str):
+            self.name = name
         else:
-            raise ValueError('Координаты точки должны быть числами')
-    def get_coord(self):
-        return self.__x, self.__y
-pt = Point(4.56, 2.34)    # обращение к классу
-print(pt.get_coord())    # вывод: (4.56, 2.34)
-pt.set_coord(9, '9')    # обращение к методу
-print(pt.get_coord())    # вывод ValueError:
+            raise ValueError("Тип имени должен быть строковым.")
+
+    def set_count_products(self, count_products: int):
+        if isinstance(count_products, int) and count_products >= 0:
+            self.count_products = count_products
+        else:
+            raise ValueError("Количество изделий должно быть целым числом и положительным.")
+
+    def set_avg_price(self, avg_price: (int, float)):
+        if isinstance(avg_price, (int, float)) and avg_price >= 0:
+            self.avg_price = avg_price
+        else:
+            raise ValueError("Средняя цена на изделие должна быть либо целым,либо дробным числом и больше 0.")
+
+    def get_name(self):
+        return self.name
+
+    def get_count_product(self):
+        return self.count_products
+
+    def get_avg_price(self):
+        return self.avg_price
+
+
+a = Bakery('R', -100, 1000)
+b = Bakery('Bread', 1, 500)
+print(a.get_name(), a.get_count_product(), a.get_avg_price())
+print(b.get_name(), b.get_count_product(), b.get_avg_price())
